e6e326ed2b730ab3364af11e380ca9364b14ee46
Acynosura <andromedhacynosura@mail.ugm.ac.id>
2023-08-22 15:15:22 +0700

    second commit

---
+++ app/src/main/java/com/example/pert22/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pert22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.pert22.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnTest1.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Hello ABCCDujuj", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
